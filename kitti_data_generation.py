2a5cf687d4e0e96bb8160d47f42a0ec65fda8fa8
SiyuanPengUMD <48134935+SiyuanPengUMD@users.noreply.github.com>
2019-06-06 15:15:10 -0400

    Update kitti_data_generation.py

--- kitti_data_generation.py
+++ kitti_data_generation.py
@@ -4,6 +4,7 @@ import glob
 import numpy as np
 import seg
 import tensorflow as tf
+import sys
 
 class PreProcessor():
     def __init__(self,input_dir, output_dir):
@@ -22,14 +23,16 @@ class PreProcessor():
         # define default input and output directory
         if input_dir is None:
             self.input_dir = INPUT_DIR
+        else:
+            self.input_dir = input_dir
         if output_dir is None:
             self.output_dir = OUTPUT_DIR
         else:
-            self.input_dir = input_dir
             self.output_dir = output_dir
             
         # if input directory not found, return error.    
         if not os.path.isdir(self.input_dir):
+            print("Incorrect Input Directory: "+ self.input_dir)
             return FileNotFoundError("X incorrect input directory")
         # if output directory is not found, create a new directory
         if not os.path.isdir(self.output_dir):
@@ -44,7 +47,7 @@ class PreProcessor():
         curr_img_list: list = glob.glob(self.input_dir+"image_2/*.png")
         curr_img_list = sorted(curr_img_list)
 
-        for i in range(8): #len(curr_img_list)
+        for i in range(len(curr_img_list)): #len(curr_img_list)
             # creating segmentation object
             segger = seg.SegGen(self.SEQ_LENGTH) 
             # create a list that will contains all frames in resized form (np array)
@@ -66,7 +69,8 @@ class PreProcessor():
                 frame_list.append(prev_img)
                 prev_img = cv2.resize(prev_img, (self.WIDTH, self.HEIGHT))
                 large_img[:, (self.NUM_PREV_FRAM - num_of_fram-1) * self.WIDTH: (self.NUM_PREV_FRAM - num_of_fram) * self.WIDTH] = prev_img
-                
+            # output big img
+            cv2.imwrite(self.output_dir + seq + ".png", large_img)
     
             # get segmentation for all frames here
             seg_list = segger.generate_segmentation(frame_list)
@@ -78,20 +82,27 @@ class PreProcessor():
             for j in range(self.NUM_PREV_FRAM):
                 mask = cv2.resize(seg_list[j+1], (self.WIDTH, self.HEIGHT))
                 large_mask[:, (self.NUM_PREV_FRAM - j-1) * self.WIDTH: (self.NUM_PREV_FRAM - j) * self.WIDTH] = mask
-                
-            # output big img
-            cv2.imwrite(self.output_dir + seq + ".png", large_img)
+            
+            # output big segmask
             cv2.imwrite(self.output_dir+ seq + "-seg.png", large_mask)
         
         print("Large img and large seg generation is done")
 
         return
-
+    
 if __name__ == '__main__':
-    print(*** staring segmentation generation ***)
-    pre_processor = PreProcessor(None, None)
+    input_dir_sys = None
+    output_dir_sys = None
+    
+    if len(sys.argv) > 1:
+        input_dir_sys = sys.argv[1]
+    if len(sys.argv) > 2:
+        output_dir_sys = sys.argv[2]
+
+    print("*** starting segmentation generation ***")
+    pre_processor = PreProcessor(input_dir_sys, output_dir_sys)
     pre_processor.data_gen()
-    print(*** end of segmentation generation ***)
+    print("*** end of segmentation generation ***" )
 
 
 
